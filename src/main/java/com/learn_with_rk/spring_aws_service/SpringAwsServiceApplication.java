924369e1dc3520831384efaaa87e847737cb34a6
Radha Krishna <rkkasiraju@gmail.com>
2024-07-03 18:51:50 +0530

    Initial Commit

---
+++ src/main/java/com/learn_with_rk/spring_aws_service/SpringAwsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learn_with_rk.spring_aws_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn_with_rk/spring_aws_service/SpringAwsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_with_rk.spring_aws_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
